6aa21ee4015b708403e48ad08a82e4de27a3ec29
pccontrol <pccontrol.wordpress@gmail.com>
2024-03-19 07:01:29 +0700

    Update display7segment.ino

--- examples/display7segment/display7segment.ino
+++ examples/display7segment/display7segment.ino
@@ -1,1 +1,18 @@
 
+#include "Segment3inch.h"
+
+Segment3inch displayku(6,7,8); //clock-strobe-data
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+ {
+  // put your main code here, to run repeatedly:
+displayku.writeAngka(123,4);
+displayku.writeAngka(456,4);
+displayku.writeAngka(789,4);
+displayku.strobe_74595();
+}
